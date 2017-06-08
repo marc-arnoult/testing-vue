530ffe41ee67dff2d39c6482287314f0f14d68fd
marc <marc.arnoult@hotmail.fr>
2017-06-08 18:21:42 +0200

    adding front-end to the api

---
+++ front/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    Hello there : {{ msg }}
+
+  </div>
+</template>
+
+<script>
+
+import axios from 'axios'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      post: null
+    }
+  },
+  created() {
+    axios.get('http://localhost:8000/articles/1').then((res) => {
+      this.post = res.data
+    })
+  }
+}
+</script>
+
+<style>
+
+</style>
